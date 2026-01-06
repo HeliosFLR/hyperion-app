b0109982408981b4dba30492582774e7a2ae9f05
Hyperion Team <hyperionfinancesolana@proton.me>
2026-01-06 01:56:21 +0100

    Update social links - remove Dune, fix GitHub and Telegram

--- src/lib/components/SocialLinks.svelte
+++ src/lib/components/SocialLinks.svelte
@@ -2,7 +2,6 @@
 	import xLogo from '$lib/images/x-twitter.svg';
 	import telegramLogo from '$lib/images/telegram.svg';
 	import githubLogo from '$lib/images/github.svg';
-	import duneLogo from '$lib/images/dune.svg';
 </script>
 
 <div class="flex flex-row gap-4">
@@ -14,8 +13,5 @@
 	</a>
 	<a href="https://github.com/HeliosFLR/hyperion-app" target="_blank">
 		<img src={githubLogo} alt="github" class="h-4 w-4 sm:h-5 sm:w-5" />
-	</a>
-	<a href="https://dune.com/hyperionfinance" target="_blank">
-		<img src={duneLogo} alt="dune" class="h-4 w-4 sm:h-5 sm:w-5" />
 	</a>
 </div>
